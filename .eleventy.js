357df4fddb5f129782292ea894c6e2e5ea9769cb
Zarema Khalilova <zarema.khalilova@gmail.com>
2024-09-24 20:58:33 +0400

    Initial version of website

---
+++ .eleventy.js
@@ -0,0 +1,22 @@
+const htmlmin = require("html-minifier");
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
+    if (outputPath && outputPath.endsWith(".html")) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true
+      });
+      return minified;
+    }
+    return content;
+  });
+
+  return {
+    dir: {
+      input: "src",
+      output: "dist"
+    }
+  };
+};
